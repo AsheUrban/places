082366dcddcc9d6ca820a454662e80f53e947540
Ashe Urban <ashe@goldentongue.com>
2022-06-13 14:43:39 -0700

    WIP UI Logic, first pass.
    Co-authored-by: Manuel Raygoza <manny.raygoza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@ function PassPort () {
 
 PassPort.prototype.addPlace = function(place) {
   this.places[place.location] = place;
+  return this.place;
 };
 
 
-// Business Log for Places ------
+// Business Logic for Places ------
 function Place (location, landmarks, season, notes) {
   this.location = location;
   this.landmarks = landmarks;
@@ -19,3 +20,17 @@ function Place (location, landmarks, season, notes) {
 let passPort = new PassPort();
 let place = new Place("Paris", "Eiffel Tower", "Spring", "Too many people");
 passPort.addPlace(place);
+
+// UI Logic for PassPort --------
+$(document).ready(function() {
+  $("form#pass-port").submit(function(event) {
+    event.preventDefault();
+  // gather values from input fields
+  const location = $("#location").val();
+  const landmark = $("#landmark").val();
+  const season = $("#season").val();
+  const notes = $("#notes").val();
+  // output results
+  })
+})
+
